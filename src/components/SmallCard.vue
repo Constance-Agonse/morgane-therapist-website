eb414b1d1b489e8d9bc68c2e5637579c469e570e
Constance <constance@MacBook-Pro-de-Constance.local>
2024-01-27 14:27:31 +0100

    feat(SmallCard): setup small card component

---
+++ src/components/SmallCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="max-w-[386px] bg-white flex flex-col items-center rounded-md p-6 gap-7">
+    <div class="text-center">
+      <i class="fas fa-calendar-check"></i>
+      <h3>Title</h3>
+    </div>
+    <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto iure culpa, assumenda, earum consequatur id
+      possimus qui tempora quisquam laboriosam natus. Molestiae quas assumenda blanditiis vel ipsum enim quis sint?</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SmallCard',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+    }
+  }
+
+}
+</script>
+
