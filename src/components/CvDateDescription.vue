ce2b8c2976eb473710d11244496cd00f0490ab54
Constance <constance@MacBook-Pro-de-Constance.local>
2024-02-05 09:18:48 +0100

    feat(ProCv.vue): split CV into small components

---
+++ src/components/CvDateDescription.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex flex-row gap-12">
+    <div class="min-w-[110px]">{{line.date}}</div>
+    <div>{{line.content}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CvDateDescription',
+  props: {
+    line: Object,
+  },
+}
+</script>
+
--- src/components/ProCV.vue
+++ src/components/ProCV.vue
@@ -2,61 +2,34 @@
   <div class="flex flex-col gap-4">
     <div class="flex flex-col gap-3">
       <div class="font-title">Diplômes nationaux et universitaires</div>
-      <div class="flex flex-row gap-12">
-        <div class="min-w-[110px]">2015</div>
-        <div>Master en psychologie : psychopathologie clinique psychanalytique - Parcours clinique et
-          psychopathologies dans le domaine médical - École de Psychologues Praticiens (EPP)</div>
-      </div>
+      <CvDateDescription :line="contentCv.diplomas[0]" />
     </div>
 
     <div class="flex flex-col gap-3">
       <div class="font-title">Autres formations</div>
-      <div class="flex flex-row gap-12">
-        <div class="min-w-[110px]">Depuis 2022</div>
-        <div>Formation continue à l'APRTF (Association pour la recherche et le travail avec les familles)</div>
-      </div>
-      <div class="flex flex-row gap-12">
-        <div class="min-w-[110px]">2018</div>
-        <div>Attestation de formation au psychodrame psychanalytique individuel - Cerep-Phymentin</div>
-      </div>
+      <CvDateDescription v-for="(course, index) in contentCv.courses" :key="index" :line="course" />
     </div>
-    <div v-if="showMoreInfo" class="flex flex-col gap-3">
-      <div class="font-title">Expériences</div>
-      <div class="flex flex-row gap-12">
-        <div class="min-w-[110px]">2021 - 2022</div>
-        <div>Praticienne - Hôpital Delafontaine - Saint-Denis - Centre médico-psychologique (CMP) pour enfants
-        </div>
-      </div>
-      <div class="flex flex-row gap-12">
-        <div class="min-w-[110px]">2019 - 2022</div>
-        <div>Praticienne - Association Cerep-Phymentin - Paris - Hôpital de jour (HDJ) pour adolescents</div>
-      </div>
-      <div class="flex flex-row gap-12">
-        <div class="min-w-[110px]">2016 - 2021</div>
-        <div>Praticienne - Hôpital André Mignot - Le Chesnay - Centre médico-psychologique (CMP)</div>
-      </div>
-      <div class="flex flex-row gap-12">
-        <div class="min-w-[110px]">2016 - 2019</div>
-        <div>Praticienne - Université de Versailles-Saint-Quentin-en-Yvelines (UVSQ) - Versailles - Service de médecine
-          préventive et de promotion de la santé (SUMPPS)</div>
-      </div>
-      <div class="flex flex-row gap-12">
-        <div class="min-w-[110px]">2015 - 2016</div>
-        <div>Praticienne - Réseau de Promotion pour la Santé Mentale 78 Sud (RPSM 78) - Le Chesnay-Rocquencourt - Maison
-          des adolescents (MDA) de Versailles</div>
-      </div>
-    </div>
-    <button
-      class="text-dark-green absolute right-10 bottom-3 text-small transition-all duration-300 ease-in-out hover:scale-[1.025] hover:text-darker-green hover:underline"
-      @click="showMoreContent()">{{ showMoreInfo ? 'Voir moins' : 'Voir plus' }}</button>
   </div>
+  <div v-if="showMoreInfo" class="flex flex-col gap-3">
+    <div class="font-title">Expériences</div>
+    <CvDateDescription v-for="(experience, index) in contentCv.experiences" :key="index" :line="experience" />
+  </div>
+  <button
+    class="text-dark-green absolute right-10 bottom-3 text-small transition-all duration-300 ease-in-out hover:scale-[1.025] hover:text-darker-green hover:underline"
+    @click="showMoreContent()">{{ showMoreInfo ? 'Voir moins' : 'Voir plus' }}</button>
 </template>
 
 <script>
+import contentCv from '@/assets/CvContent.json'
+import CvDateDescription from '@/components/CvDateDescription.vue'
 export default {
   name: 'ProCV',
+  components: { CvDateDescription },
   data() {
-    return { showMoreInfo: false }
+    return {
+      showMoreInfo: false,
+      contentCv,
+    }
   },
   methods: {
     showMoreContent() {
