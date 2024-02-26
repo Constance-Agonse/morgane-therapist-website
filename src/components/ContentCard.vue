f3ab750e2ee186f426c3e41120de17d6b04f23d9
Constance <constance@MacBook-Pro-de-Constance.local>
2024-02-26 20:22:46 +0100

    feat(ContentCard.vue): add more padding if seeMore btn

--- src/components/ContentCard.vue
+++ src/components/ContentCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="font-body">
-    <div class="max-w-[850px] bg-white rounded-md p-6">
+    <div class="max-w-[850px] bg-white rounded-md p-6" :class="{'pb-8' : thematic.seeMore}">
       <h1 class="font-title pb-4">{{ thematic.title }}</h1>
       <div v-for="message in thematic.content" :key="message.index" class="pb-3">
         {{ message }}
