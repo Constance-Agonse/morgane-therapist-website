6c05a72bae2bd369bbb3f1bba6acdae5bf9a06b8
Constance <constance@MacBook-Pro-de-Constance.local>
2024-03-16 14:48:32 +0100

    fix(landing-page desktop): fix animation

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="px-28 bg-light-green font-title text-dark-green text-4xl flex items-center leading-relaxed" 
     :style="{ height: `${height}px` }">
-    <h1 id="text" v-motion-slide-visible-once-top>
+    <h1 id="text" v-motion-slide-top>
       Psychologue diplômée et psychothérapeute, j'accueille les adultes et également les jeunes enfants et les
       adolescents,
       sur rendez-vous au cabinet.
