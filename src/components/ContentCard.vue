ee622aa560f8d9d4c6f024e470d06b4f8a08b4ed
Constance <constance@MacBook-Pro-de-Constance.local>
2024-06-01 16:19:31 +0200

    feat(text): change content section

--- src/components/ContentCard.vue
+++ src/components/ContentCard.vue
@@ -2,8 +2,7 @@
   <div class="font-body">
     <div class="max-w-[850px] bg-white rounded-md p-6" :class="{'pb-8' : thematic.seeMore}">
       <h1 class="font-title pb-4">{{ thematic.title }}</h1>
-      <div v-for="message in thematic.content" :key="message.index" class="pb-3">
-        {{ message }}
+      <div v-for="message in thematic.content" :key="message.index" class="pb-3" v-html="message">
       </div>
       <slot></slot>
     </div>
@@ -18,3 +17,12 @@ export default {
   }
 }
 </script>
+
+<style>
+ul.consultation-list {
+  list-style-type: disc;
+  padding-left: 1.5rem;
+  margin-top: 0.5rem;
+  margin-bottom: 0.5rem;
+}
+</style>
