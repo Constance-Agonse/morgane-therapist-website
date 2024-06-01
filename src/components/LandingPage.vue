d28f8d06b62bfe21e5c2c40497c691a63c0051b8
Constance <constance@MacBook-Pro-de-Constance.local>
2024-06-01 13:13:03 +0200

    feat(text): change intro title

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,10 +1,8 @@
 <template>
-  <div class="px-28 bg-light-green font-title text-dark-green text-4xl flex items-center leading-relaxed" 
+  <div class="bg-light-green font-title text-dark-green text-4xl flex items-center leading-relaxed" 
     :style="{ height: `${height}px` }">
     <h1 id="text" v-motion-slide-top>
-      Psychologue diplômée et psychothérapeute, j'accueille les adultes et également les jeunes enfants et les
-      adolescents,
-      sur rendez-vous au cabinet.
+      Psychologue diplômée et psychothérapeute, j'accueille les adultes, les adolescents ainsi que les jeunes enfants, sur rendez-vous au cabinet. J’accueille également les familles, le(s) parent(s) avec leur bébé et les couples.
     </h1>
 
   </div>
